31fffc04cd28aedd210669ad87e44c8958acb34c
Nacho <ignacio.rodriguez@evoluciona.com>
2025-04-28 10:52:30 +0200

    no message

--- src/middlewares/socket.js
+++ src/middlewares/socket.js
@@ -1,21 +1,19 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log('Nuevo cliente conectado');
-
     socket.on('kudo', (kudo) => {
-      console.log('Recibido kudo:', kudo);
       socket.broadcast.emit('kudo', kudo);
     });
 
     socket.on('cambiar-coleccion', (data) => {
-      console.log('Recibido cambio de colección:', data);
       const { coleccion } = data;
-      // Emitir a todos los clientes excepto al emisor, igual que con los kudos
       socket.broadcast.emit('cambiar-coleccion', { coleccion });
     });
 
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado');
+    socket.on('audio-data', (data) => {
+      console.log('[AUDIO] Recibido audio-data', data);
+      socket.broadcast.emit('audio-data', data);
     });
+
+    socket.on('disconnect', () => {});
   });
 }; 
